d9632db32e21c84dfeb3d521eb7bab1399fc297a
Ivan Alejandro <ivanalejandro0@yahoo.com.ar>
2010-10-30 16:34:29 -0300

    Add licensing for GPL v3

--- __init__.py
+++ __init__.py
@@ -1,5 +1,28 @@
-# Widget Traductor de idiomas, usando la API de Google Translate
+# -*- coding: utf-8 -*-
+# This file is part of QTranslate.
+#
+# QTranslate is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# QTranslate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QTranslate.  If not, see <http://www.gnu.org/licenses/>.
 
+
+# Español / Spanish
+# Traductor de idiomas, usando la API de Google Translate
 # Inspirado en el videotutorial de TuxRadar.com:
+# "Code Project: build a PyGTK language translator"
+# (http://www.tuxradar.com/content/code-project-build-pygtk-language-translator)
+
+# English / Ingles
+# Language translator using Google Translate API
+# Inspired in TuxRadar.com's videotutorial
 # "Code Project: build a PyGTK language translator"
 # (http://www.tuxradar.com/content/code-project-build-pygtk-language-translator)
--- qtranslate.py
+++ qtranslate.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# This file is part of QTranslate.
+#
+# QTranslate is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# QTranslate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QTranslate.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import os
 import sys
 
@@ -20,6 +36,7 @@ class QTranslate(QtGui.QMainWindow):
         uifile = os.path.join(
             os.path.abspath(os.path.dirname(__file__)), "qtranslate.ui")
 
+        uifile = 'qtranslate.ui'
         uic.loadUi(uifile, self)
 
         self.ui = self
